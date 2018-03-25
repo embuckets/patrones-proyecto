0ef47c08e9f62ec5cf363262f5815755e5e06c5b
emilio <mok.boss@hotmail.com>
2018-03-25 15:30:28 -0600

    terminar implementar NumeroDAO

--- Gestion-Clientes/src/modelo/Cliente.java
+++ Gestion-Clientes/src/modelo/Cliente.java
@@ -5,37 +5,44 @@ public class Cliente {
 	private String nombre;
 	private String paterno;
 	private String materno;
-	
+
 	public Cliente(Integer id, String nombre, String paterno, String materno) {
 		this.id = id;
 		this.nombre = nombre;
 		this.paterno = paterno;
 		this.materno = materno;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getPaterno() {
 		return paterno;
 	}
+
 	public void setPaterno(String paterno) {
 		this.paterno = paterno;
 	}
+
 	public String getMaterno() {
 		return materno;
 	}
+
 	public void setMaterno(String materno) {
 		this.materno = materno;
 	}
-	
 
 }
