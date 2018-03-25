91adc34e16526495562aea1b43d86be880f7a006
emilio <mok.boss@hotmail.com>
2018-03-25 13:40:35 -0600

    avance caso asignar numero

--- Gestion-Clientes/src/modelo/Cliente.java
+++ Gestion-Clientes/src/modelo/Cliente.java
@@ -1,5 +1,41 @@
 package modelo;
 
 public class Cliente {
+	private Integer id;
+	private String nombre;
+	private String paterno;
+	private String materno;
+	
+	public Cliente(Integer id, String nombre, String paterno, String materno) {
+		this.id = id;
+		this.nombre = nombre;
+		this.paterno = paterno;
+		this.materno = materno;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPaterno() {
+		return paterno;
+	}
+	public void setPaterno(String paterno) {
+		this.paterno = paterno;
+	}
+	public String getMaterno() {
+		return materno;
+	}
+	public void setMaterno(String materno) {
+		this.materno = materno;
+	}
+	
 
 }
--- Gestion-Clientes/src/modelo/NumeroDAO.java
+++ Gestion-Clientes/src/modelo/NumeroDAO.java
@@ -1,6 +1,8 @@
 package modelo;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class NumeroDAO {
 	public Numero buscarNumero(String numero) {
@@ -26,4 +28,113 @@ public class NumeroDAO {
 		}
 		return numeroARegresar;
 	}
+	
+	public Numero buscarDisponible() {
+		Numero numeroARegresar = null;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from patrones.numero where estado=" + "'" + "disponible" + "'" + " limit 1";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			if (resultSet.next()) {
+				String resultNumero = resultSet.getString("numero");
+				Numero.Estado estado = Numero.Estado.valueOf(resultSet.getString("estado"));
+				numeroARegresar = new Numero(resultNumero, estado);
+
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return numeroARegresar;
+	}
+	
+	public ArrayList<Numero> buscarNumeroDelCliente(Integer id_cliente) {
+		ArrayList<Numero> numerosDelCliente = new ArrayList<Numero>();
+		
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from patrones.numero_asignado where id_cliente=" + id_cliente;
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while (resultSet.next()) {
+				String resultNumero = resultSet.getString("numero");
+				Numero numeroNuevo = new Numero(resultNumero, Numero.Estado.asignado);
+				numerosDelCliente.add(numeroNuevo);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return numerosDelCliente;
+	}
+	
+	public boolean asignar (Numero numero, Cliente cliente) {
+		int updateNumeroSuccessful = 0, insertNumeroAsignadoSuccessful = 0;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			numero.setEstado(Numero.Estado.asignado);
+			
+			String updateNumero = "update patrones.numero set estado = '" + numero.getEstado() + "' where numero= " + "'" + numero.getNumero() + "';";
+			statement = conn.createStatement();
+			updateNumeroSuccessful = statement.executeUpdate(updateNumero);
+			
+			String insertNumeroAsignado = "insert into patrones.numero_asignado values (" + "'" + numero.getNumero() + "'" + "," 
+					+ cliente.getId() + ");";
+			statement = conn.createStatement();
+			insertNumeroAsignadoSuccessful = statement.executeUpdate(insertNumeroAsignado);
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return (insertNumeroAsignadoSuccessful > 0 && updateNumeroSuccessful > 0);
+	}
+	
+	public boolean darDeBaja(Numero numero) {
+		int deleteNumeroAsignadoSuccessful = 0, updateNumeroSuccessful = 0, insertNumeroBajaSuccessful = 0;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			numero.setEstado(Numero.Estado.baja);
+			
+			String updateNumero = "update patrones.numero set estado = '" + numero.getEstado() + "' where numero= '" + numero.getNumero() + "';";
+			statement = conn.createStatement();
+			updateNumeroSuccessful = statement.executeUpdate(updateNumero);
+			
+			String deleteNumeroAsignado = "delete from patrones.numero_asignado where numero = '" + numero.getNumero() + "';";
+			statement = conn.createStatement();
+			deleteNumeroAsignadoSuccessful = statement.executeUpdate(deleteNumeroAsignado);
+			
+			
+			Calendar ahora = Calendar.getInstance();
+			
+			String insertNumeroBaja = "insert into patrones.numero_baja values (" + numero.getNumero() + ");";
+			statement = conn.createStatement();
+			insertNumeroBajaSuccessful = statement.executeUpdate(insertNumeroBaja);
+			
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		
+		
+		return (deleteNumeroAsignadoSuccessful > 0 && updateNumeroSuccessful > 0 && insertNumeroBajaSuccessful > 0);
+	}
+
+	
+	
+	
+	
 }
