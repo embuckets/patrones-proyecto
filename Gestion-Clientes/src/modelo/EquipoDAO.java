2a391546f88524c0075aaa51f7cb443feb00ffdd
emilio <mok.boss@hotmail.com>
2018-03-26 10:06:50 -0600

    agregar fuente del caso asignar equipo

--- Gestion-Clientes/src/modelo/EquipoDAO.java
+++ Gestion-Clientes/src/modelo/EquipoDAO.java
@@ -1,70 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.*;
+import java.util.LinkedList;
 
-
+/**
+ *
+ * @author Christian
+ */
 public class EquipoDAO {
-	/*
-	public Equipo buscarModelo(Integer modeloId) {
-		//crear conexion
-		try {
-			DataConnector dataConnector = new DataConnector();
-			Connection connection = dataConnector.getConnection();
-			Statement statement = null;
-			String query = "select * from patrones.equipo where id_equipo = " + modeloId;
 
-			
-			
+	public LinkedList<Equipo> listaEquipos() {
+
+		LinkedList<Equipo> lista = new LinkedList();
+
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from equipo, equipo_inventario where equipo.id_equipo = equipo_inventario.id_equipo";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			while (resultSet.next()) {
+				String modelo = resultSet.getString("modelo");
+				String color = resultSet.getString("equipo_inventario.color");
+				String sistemaOperativo = resultSet.getString("sistema_operativo");
+				float precio = resultSet.getFloat("precio");
+				String fabricante = resultSet.getString("fabricante");
+				Equipo equipo = new Equipo(modelo, color, sistemaOperativo, precio, fabricante);
+				lista.add(equipo);
+			}
+			conn.close();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			// TODO: handle exception
-		} finally {
-			
 		}
-		
-		//construir la sentencia query
-		//que busque modeloId en la tabla equipo
-		//ejecutar query
-		//si se encontro un resultado 
-		//	regresarlo
-		//si no
-		//	mandar error u objeto nulo
-
+		return lista;
 	}
 
-	public ArrayList<Equipo> buscarPorPrecioMenorA(Float precio) {
+	public EquipoCliente buscarEquipo(String modelo, String color) {
+		EquipoCliente equipo = null;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from equipo, equipo_inventario where " + "modelo='" + modelo + "' and color='"
+					+ color + "'";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
 
+			if (resultSet.next()) {
+				int id = resultSet.getInt("equipo.id_equipo");
+				String col = resultSet.getString("color");
+				float precio = resultSet.getFloat("precio");
+				equipo = new EquipoCliente(0, 0, id, col, precio, null);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return equipo;
 	}
 
-	public ArrayList<Equipo> buscarPorPrecioMayorA(Float precio) {
+	public boolean asignar(EquipoCliente equipo) {
 
+		boolean bandera = false;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "insert into equipo_compra values(null, '" + equipo.getFecha() + "', "
+					+ equipo.getIdCliente() + ", " + equipo.getIdEquipo() + ", '" + equipo.getColor() + "', "
+					+ equipo.getPrecio() + ")";
+			statement = conn.createStatement();
+			statement.executeUpdate(query);
+			conn.close();
+			bandera = true;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return bandera;
 	}
 
-	public ArrayList<Equipo> buscarEntrePrecio(Float cotaInferior, Float coraSuperior) {
-
-	}
-
-	public ArrayList<Equipo> buscarPorFabricante(String fabricante) {
-
-	}
-
-	public ArrayList<Equipo> buscarPorSistemaOperativo(String sistemaOperativo) {
-
-	}
-
-	public ArrayList<Equipo> buscarExistencias(Integer modeloId) {
-
-	}
-
-	public ArrayList<Equipo> buscarModelo(String modelo) {
-
-	}
-
-	public boolean asignar(Integer modeloId, String color, Integer clienteId) {
-
-	}
-*/
 }
