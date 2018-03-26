3fb7f74974dd4cd9d6c469031af23f2d9cdb3028
emilio <mok.boss@hotmail.com>
2018-03-25 19:36:17 -0600

    avance asignar numero teminar implementar asignarNumeroMainController

--- Gestion-Clientes/src/controlador/AsignarNumeroMainController.java
+++ Gestion-Clientes/src/controlador/AsignarNumeroMainController.java
@@ -1,46 +1,107 @@
 package controlador;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
 import modelo.Cliente;
+import modelo.ClienteDAO;
 import modelo.Numero;
+import modelo.NumeroDAO;
 
-public class AsignarNumeroMainController {
+public class AsignarNumeroMainController extends Observable{
+	private static AsignarNumeroMainController instance;
 	private Cliente cliente;
 	private ArrayList<Numero> numerosRegistrados;
 	private ArrayList<Numero> numerosPorAsignar;
 	private ArrayList<Numero> numerosPorCancelar;
 	
-	public Cliente buscarCliente(String nombre, String paterno, String materno) {
-		return null;
+	private AsignarNumeroMainController() {
+		super();
+		cliente = null;
+		numerosRegistrados = new ArrayList<Numero>();
+		numerosPorAsignar = new ArrayList<Numero>();
+		numerosPorCancelar = new ArrayList<Numero>();
 	}
 	
-	public Cliente getCliente() {
-		return null;
+	public static AsignarNumeroMainController getInstance() {
+		if (instance == null) {
+			instance = new AsignarNumeroMainController();
+		}
+		return instance;
 	}
 	
-	public ArrayList<Numero> getNumerosRegistrados(){
-		return null;
+	public boolean buscarCliente(String nombre, String paterno, String materno) {
+		boolean success = false;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Cliente cliente = clienteDAO.buscarCliente(paterno, materno, nombre);
+		if (cliente != null) {
+			this.cliente = cliente;
+			buscarNumerosDelCliente(cliente);
+			success = true;
+			setChanged();
+			notifyObservers();
+		}
+		return success;
+		
 	}
 	
-	public ArrayList<Numero> getNumerosPorAsignar(){
-		return null;
+	public ClienteDTO getCliente() {
+		ClienteDTO clienteDTO = new ClienteDTO(cliente, numerosRegistrados, numerosPorAsignar, numerosPorCancelar);
+		return clienteDTO;
 	}
 	
-	public ArrayList<Numero> getNumerosPorCancelar(){
-		return null;
-	}
-	
-	public boolean agregarNumeroDisponible() {
-		return false;
+	public boolean buscarNumeroDisponible() {
+		boolean success = false;
+		NumeroDAO numeroDAO = new NumeroDAO();
+		Numero numero = numeroDAO.buscarDisponible();
+		if (numero != null) {
+			numerosPorAsignar.add(numero);
+			success = true;
+			setChanged();
+			notifyObservers();
+		}
+		
+		return success;
 	}
 	
 	public boolean darDeBajaNumero(String numero){
-		return false;
+		Numero numeroADarBaja = new Numero(numero, Numero.Estado.asignado);
+		boolean success = false;
+		NumeroDAO numeroDAO = new NumeroDAO();
+		boolean esDelCliente = numeroDAO.buscarSiNumeroEsDelCliente(numeroADarBaja, cliente);
+		if (esDelCliente ) {
+			numerosPorCancelar.add(numeroADarBaja);
+			success = true;
+			setChanged();
+			notifyObservers();
+		}
+		return success;
 	}
 	
 	public boolean guardarCambios() {
-		return false;
+		boolean success = false;
+		NumeroDAO numeroDAO = new NumeroDAO();
+		for(Numero numero : numerosPorAsignar) {
+			numeroDAO.asignar(numero, cliente);
+		}
+		
+		for(Numero numero : numerosPorCancelar) {
+			numeroDAO.darDeBaja(numero);
+		}
+		
+		setChanged();
+		notifyObservers();
+		
+		success = true;
+		return success;
+	}
+	
+	private void buscarNumerosDelCliente(Cliente cliente) {
+		NumeroDAO numeroDAO = new NumeroDAO();
+		ArrayList<Numero> numerosDelCliente = numeroDAO.buscarNumerosDelCliente(cliente);
+		for (Numero numero : numerosDelCliente) {
+			numerosRegistrados.add(numero);
+		}
 	}
 	
 	public void cancelarCambios() {
--- Gestion-Clientes/src/modelo/ClienteDAO.java
+++ Gestion-Clientes/src/modelo/ClienteDAO.java
@@ -1,5 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo;
 
+import java.sql.*;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Christian
+ */
 public class ClienteDAO {
+
+	public LinkedList<Cliente> listaClientes() {
+
+		LinkedList<Cliente> lista = new LinkedList<Cliente>();
+
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from patrones.cliente";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			while (resultSet.next()) {
+				Integer id = resultSet.getInt("id_cliente");
+				String paterno = resultSet.getString("paterno");
+				String materno = resultSet.getString("materno");
+				String nombre = resultSet.getString("nombre");
+				Cliente cliente = new Cliente(id, nombre, paterno, materno);
+				lista.add(cliente);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return lista;
+	}
+
+	public Cliente buscarCliente(Cliente idCliente) {
+		Cliente cliente = null;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from cliente where id_cliente=" + "'" + idCliente.getId() + "'";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			if (resultSet.next()) {
+				Integer id = resultSet.getInt("id_cliente");
+				String paterno = resultSet.getString("paterno");
+				String materno = resultSet.getString("materno");
+				String nombre = resultSet.getString("nombre");
+				cliente = new Cliente(id, nombre, paterno, materno);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return cliente;
+	}
+
+	public Cliente buscarCliente(String p, String m, String name) {
+		Cliente cliente = null;
+		try {
+			DataConnector connector = new DataConnector();
+			Connection conn = connector.getConnection();
+			Statement statement = null;
+			String query = "select * from cliente where paterno='" + p + "' and materno='" + m + "' and nombre='" + name
+					+ "'";
+			statement = conn.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+
+			if (resultSet.next()) {
+				Integer id = resultSet.getInt("id_cliente");
+				String paterno = resultSet.getString("paterno");
+				String materno = resultSet.getString("materno");
+				String nombre = resultSet.getString("nombre");
+				cliente = new Cliente(id, nombre, paterno, materno);
+			}
+			conn.close();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+		return cliente;
+	}
 
 }
